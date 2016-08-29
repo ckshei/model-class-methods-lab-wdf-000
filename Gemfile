2c23ce665bf04f9302d04227fbd7a249e99876e3
ckshei <cks856@gmail.com>
2016-08-29 16:31:36 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.3.0'
+ruby '2.3.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2'
@@ -37,5 +37,6 @@ group :test, :development do
   gem "rspec-rails"
   gem "factory_girl_rails"
   gem "database_cleaner"
+  gem "byebug"
   gem "pry"
 end
--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,40 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+  
+  def self.first_five
+    Boat.limit(5)  
+  end
+
+  def self.dinghy
+    Boat.where("length < ?", 20)
+  end
+
+  def self.ship
+    Boat.where("length > ?", 20)
+  end
+
+  def self.last_three_alphabetically
+    Boat.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    Boat.where(captain_id: nil)
+  end
+
+  def self.sailboats
+    sailboat_id = Classification.find_by_name('Sailboat').id
+    boat_ids = BoatClassification.where(classification_id: sailboat_id).pluck(:boat_id)
+    Boat.where(id: boat_ids)
+  end
+
+  def self.with_three_classifications
+    boat_ids = BoatClassification.group(:boat_id).having("count(boat_id) > 2").pluck(:boat_id)
+    Boat.where(id: boat_ids)
+  end
+
+  def self.longest
+    order('length DESC').first
+  end
 end
+
